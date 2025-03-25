87c50e6a9433cd4c8885936acf86d9471322e96b
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2025-03-25 21:32:32 +0530

    BankForm create

---
+++ src/components/BankForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const BankForm = () => {
+  return <div>BankForm</div>;
+};
+
+export default BankForm;
