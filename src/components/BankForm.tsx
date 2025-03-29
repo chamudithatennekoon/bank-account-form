d25f20e84262f9ebcffad8d4c0a5fb22cc92ea57
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2025-03-29 22:34:04 +0530

    add MUI to form

--- src/components/BankForm.tsx
+++ src/components/BankForm.tsx
@@ -2,9 +2,31 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import {
+  TextField,
+  Button,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Container,
+  Paper,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 const BankForm = () => {
-  return <div>BankForm</div>;
+  return (
+    <>
+      <Container maxWidth="sm"></Container>
+    </>
+  );
 };
 
 export default BankForm;
